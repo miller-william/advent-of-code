104ba6b532c15d34d577de4fd306295d0a58396d
miller-william <77627423+miller-william@users.noreply.github.com>
2021-12-03 10:24:34 +0000

    day 3 comments

--- 03/R-03-advent.R
+++ 03/R-03-advent.R
@@ -8,8 +8,11 @@ gamma <- ""
 epsilon <- ""
 
 for(i in 1:12){
+  #create new columns for each position
   data[,i+1] <- substr(data[,'full'],i,i)
+  #name new columns
   names(data)[names(data) == paste0("V",i+1)] <- paste0("pos_",i)
+  #check which is most common in each position and add 1 or 0 to gamme and epsilon strings as appropriate
   if(sum(as.numeric(data[,i+1]))/nrow(data) >= 0.5){
          gamma <- paste(gamma,"1",sep = "")
          epsilon <- paste(epsilon,"0",sep = "")}
@@ -26,16 +29,20 @@ print(answer)
 data2 <- data
 #Part 2
 for(i in 1:12){
+  #Set column (ie position) we are looking at
   col <- paste0('pos_',i)
+  #filter to only rows with most common value
   if(sum(as.numeric(data2[,col]))/nrow(data2) >= 0.5){
+    #!! injection operator used to pass col string as object which can be used in filter
    data2 <- filter(data2,!!as.symbol(col)==1)}
   else{data2 <- filter(data2,!!as.symbol(col)==0)}
+  #stop process when we only have 
   if (nrow(data2)==1) {break}
   }
 
 oxygen_gen_rating <- strtoi(data2[1,1], base = 2)
 
-#co2 rating
+#co2 rating - same loop as above but take least common value in each column
 data3 <- data
 #Part 2
 for(i in 1:12){
