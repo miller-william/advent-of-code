2be2ca94e73d8c89741c201d160b8ec8a364f2ea
miller-william <77627423+miller-william@users.noreply.github.com>
2021-12-05 15:07:53 +0000

    simplifying day 05

--- 05/R-05-advent.R
+++ 05/R-05-advent.R
@@ -10,9 +10,8 @@ colnames(data2) <- c('x2','y2')
 data <- cbind(data1,data2)
 data[] <- lapply(data, function(x) as.numeric(x))
 
-#for two sets of x,y co-ords, output integer points on line between - this could be simplified
+#for two sets of x,y co-ords, output integer points on line between
 draw_line <- function(x1,x2,y1,y2){
-  #calculate gradient for us in cases which are not x1=x2 or y1=y2
     x=seq(x1,x2, length.out = 1 + max(abs(y2-y1),abs(x2-x1)))
     y=seq(y1,y2, length.out = 1 + max(abs(y2-y1),abs(x2-x1)))
     #Output list of coords
